ffe19f1076c3d0a5808d28e905bbfc09773934fe
Swarup Mahapatra <swarupmahapatra1@gmail.com>
2022-04-12 12:19:41 +0530

    Refactor command executor

--- src/main/java/com/swarup/geektrust/makespace/CommandExecutor.java
+++ src/main/java/com/swarup/geektrust/makespace/CommandExecutor.java
@@ -3,33 +3,44 @@ package com.swarup.geektrust.makespace;
 import com.swarup.geektrust.makespace.errors.InvalidBookingTimeRange;
 import com.swarup.geektrust.makespace.errors.InvalidTime;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+
 import static com.swarup.geektrust.makespace.support.CONSOLE_STRINGS.INCORRECT_INPUT;
 
 public class CommandExecutor {
     private RoomBookingService service;
+    private HashMap<String, Function<List<String>, String>> stringBlahHashMap = new HashMap<>();
+
 
     public CommandExecutor(RoomBookingService roomBookingService) {
 
         this.service = roomBookingService;
+        registerCommands();
+    }
+
+    private void registerCommands() {
+        stringBlahHashMap.put("VACANCY", this::vacancy);
+        stringBlahHashMap.put("BOOK", this::book);
     }
 
     public void execute(String[] data) {
         try {
-            executeCommands(data);
+            String command = data[0];
+            String res = stringBlahHashMap.get(command).apply(List.of(data));
+            System.out.println(res);
         }catch (InvalidTime | InvalidBookingTimeRange t) {
             System.out.println(INCORRECT_INPUT);
         }
     }
 
-    private void executeCommands(String[] data) {
-        String command = data[0];
-        if (command.equals("VACANCY")){
-            String result = service.checkVacancy(new BookingTimeRange(new Time(data[1]), new Time(data[2])));
-            System.out.println(result);
-        }
-        if (command.equals("BOOK")){
-            String result = service.book(new BookingTimeRange(new Time(data[1]), new Time(data[2])),Integer.parseInt(data[3]));
-            System.out.println(result);
-        }
+    private String vacancy(List<String> data) {
+        return service.checkVacancy(new BookingTimeRange(new Time(data.get(1)), new Time(data.get(2))));
     }
+
+    private String book(List<String> data) {
+        return service.book(new BookingTimeRange(new Time(data.get(1)), new Time(data.get(2))), Integer.parseInt(data.get(3)));
+    }
+
 }
